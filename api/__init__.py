3726cf71426337db19c1aaded6ac8659d51066ac
muchumi <muchumimaina94@gmail.com>
2023-06-18 05:07:10 +0300

    User endpoint done

--- api/__init__.py
+++ api/__init__.py
@@ -11,7 +11,8 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS = False
         )
     else:
         app.config.from_mapping(test_config)
@@ -28,4 +29,4 @@ def create_app(test_config=None):
     return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app.run(debug=True)
--- api/views/views.py
+++ api/views/views.py
@@ -1,6 +1,9 @@
+import validators
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
-from api.constants.status_codes import HTTP_400_BAD_REQUEST
+from api.constants.status_codes import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+from api.models.models import User, Bookmark, db
+
 
 users = Blueprint("users", __name__, url_prefix="/api/v1/users")
 
@@ -18,13 +21,13 @@ def register():
     email = request.json['email']
     password = request.json['password']
 
-    #Check if password is long or equal to 8 characters
+    #Check if password is longer or equal to 8 characters
     if len(password) < 8:
         return jsonify({
             "error": "Password is too short"
         }), HTTP_400_BAD_REQUEST
     
-    #Check if username is long than 4 characters
+    #Check if username is longer than 4 characters
     if len(username) < 4:
         return jsonify({
             "error": "Username is too short"
@@ -36,6 +39,34 @@ def register():
             "error": "Username should be Alpha numeric and no spaces allowed"
         }), HTTP_400_BAD_REQUEST
 
+    #Validating the email input
+    if not validators.email(email):
+        return jsonify({
+            "error": "Email is not valid"
+        }), HTTP_400_BAD_REQUEST
     
+    # Checking if the email input is unique
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({
+            "error": "Email already taken"
+        }), HTTP_409_CONFLICT
+    
+    # Checking if the username input is unique
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({
+            "error": "Username already taken"
+        }), HTTP_409_CONFLICT
+    
+    # Hashing the user password
+    password_hash = generate_password_hash(password)
 
+    user = User(username=username, email=email, password=password_hash)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({
+        "message": "User created",
+        "user": {
+            "username": username, "email": email
+        }
+    }), HTTP_201_CREATED
 
